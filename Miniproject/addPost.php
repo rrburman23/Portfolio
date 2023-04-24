c06220dc0ad04126d04000986bbd0afceb16dd93
RoloMaster23 <92298959+Rohan-Burman@users.noreply.github.com>
2023-04-24 22:36:39 +0100

    phase 2 fixing

---
+++ Miniproject/addPost.php
@@ -0,0 +1,18 @@
+<?php
+$title = $_POST['title'];
+$description = $_POST['description'];
+$author = $_POST['author'];
+$content = $_POST['content'];
+date_default_timezone_set('London');
+$date = date("d/m/y");
+
+$sql = "SELECT * FROM blog WHERE title = '$title'";
+result = mysqli_query($conn, $sql);
+ $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+ $active = $row['active'];
+ $count = mysqli_num_rows($result);
+
+ if(count==0){
+    alert("Title invalid");
+ }
+?>
--- Miniproject/login.php
+++ Miniproject/login.php
@@ -5,37 +5,27 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
 }
  require_once "config.php";
 
- $uname="";
- $pwd="";
-
-$stmt = $conn -> prepare("SELECT * FROM accounts WHERE uname = ? AND pwd = ?");
-$stmt->bind_param("ss", $uname,$pwd);
-
 $uname = mysqli_real_escape_string($conn, $_POST["username"]);  
 $pwd = mysqli_real_escape_string($conn, $_POST['pswrd']);  
 $hashPwd = password_hash($pwd, PASSWORD_DEFAULT);
-$stmt->exectue();
-
 
  $sql = "SELECT * FROM accounts WHERE uname = '$uname' AND pwd = '$pwd'";
  $result = mysqli_query($conn, $sql);
  $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
  $active = $row['active'];
  $count = mysqli_num_rows($result);
-
  
- echo $count;  
+ //echo $count;  
 
- if ($count == 0 && !(password_verify($pwd,$hashPwd))){
-   echo "Details inccorect. Please try again";
+ if ($count == 0 || !(password_verify($pwd,$hashPwd))){
+   //echo "Details inccorect. Please try again";
+   alert("Details inccorect. Please try again");
  }
  else{
    session_start();
    $_SESSION['loggedin'] = true;
    $_SESSION['senduname']=$uname;
    header("location: viewBlog.php");
- }
-
-      
+ }    
      
 ?>
