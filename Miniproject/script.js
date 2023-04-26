b6b7b569cf23c04807a5f8a171fd14a0077a7f35
RoloMaster23 <92298959+Rohan-Burman@users.noreply.github.com>
2023-04-26 04:47:11 +0100

    prettify js

--- Miniproject/script.js
+++ Miniproject/script.js
@@ -1,6 +1,19 @@
-function clear() {
+function clearTxt(){
   let msg = "Are you sure you want to clear all text input fields?";
-  if (confirm(msg)) {
-    document.getElementByClass("inputText").value = "";
+  if(confirm(msg)){-
+      document.getElementsByTagName("form")[0].reset();
   }
+}
+
+function pEmpty(){
+  let num = document.forms.length;
+  for(let i=0; i<num;i++){
+      let id = document.forms.item(i).id;
+      if(id.value=="description"){continue;}
+      else if(id.value==""||id.value==null){
+          alert("Must not be left empty");
+          return false;
+      }
+  }
+  return true;
 }
